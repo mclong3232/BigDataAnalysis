4738d39cb58ebdf53aff7bc6fd2cfea14e54b839
skywalkerog <hotsaucejalapeno@gmail.com>
2016-07-21 12:19:24 -0500

    Moved code to graph data sets from Driver.py into Graphics.py.

--- Driver.py
+++ Driver.py
@@ -16,10 +16,12 @@ username = raw_input("Username:  ")
 password = raw_input("Password:  ")
 dataB = Database.MyDatabase("localhost", username, password, "signal_intensity")
 
+
 # Clear out a database.
 # dataB.mysql.execute("DROP DATABASE `signal_intensity`;")
 # dataB.mysql.execute("CREATE SCHEMA `signal_intensity` DEFAULT CHARACTER SET utf8 ;")
 # dataB = Database.MyDatabase("localhost", username, password, "signal_intensity")
+
 
 # Get a list of all files that could be added to database
 files = dataB.get_files()
@@ -31,52 +33,21 @@ if targetfile in files:
 else:
     print "Dataset not found!"
 
+
+# Run statistical process on data sets
 # Statistics.anova()
 
-dakota.run_dakota(infile="dakota.in")
 
+# Run DAKOTA
+dakota.run_dakota(infile="dakota.in", stdout="dakota_results.out", stderr="dakota_results.err")
+
+
+# Graph the data sets
 disp_graph = raw_input("Display graphs? (Y/n)  ")
 
 if disp_graph == "Y":
-    print "Liver_UF_Control_rep2"
-    dataset = dataB.get_set("GSM576077")
-    Graphics.display_graphics(dataset)
+    Graphics.graph_sets(dataB)
 
-    print "Liver_A_Exposed_rep2"
-    dataset = dataB.get_set("GSM576083")
-    Graphics.display_graphics(dataset)
-
-    print "Liver_B_Exposed_rep2"
-    dataset = dataB.get_set("GSM576086")
-    Graphics.display_graphics(dataset)
-
-    print "Liver_D_Ref_rep2"
-    dataset = dataB.get_set("GSM576084")
-    Graphics.display_graphics(dataset)
-
-    print "Liver_E_Exposed_rep2"
-    dataset = dataB.get_set("GSM576081")
-    Graphics.display_graphics(dataset)
-
-    print "Testis_UF_Control_rep2"
-    dataset = dataB.get_set("GSM576102")
-    Graphics.display_graphics(dataset)
-
-    print "Testis_A_Exposed_rep2"
-    dataset = dataB.get_set("GSM576103")
-    Graphics.display_graphics(dataset)
-
-    print "Testis_B_Exposed_rep2"
-    dataset = dataB.get_set("GSM576106")
-    Graphics.display_graphics(dataset)
-
-    print "Testis_D_Ref_rep3"
-    dataset = dataB.get_set("GSM576100")
-    Graphics.display_graphics(dataset)
-
-    print "Testis_E_Exposed_rep2"
-    dataset = dataB.get_set("GSM576105")
-    Graphics.display_graphics(dataset)
 
 # Commit changes to database
 dataB.comm_exit()
--- Graphics.py
+++ Graphics.py
@@ -18,3 +18,46 @@ def display_graphics(dataset):
     rprint(p)
 
     raw_input()
+
+
+def graph_sets(datab):
+
+    print "Liver_UF_Control_rep2"
+    dataset = datab.get_set("GSM576077")
+    display_graphics(dataset)
+
+    print "Liver_A_Exposed_rep2"
+    dataset = datab.get_set("GSM576083")
+    display_graphics(dataset)
+
+    print "Liver_B_Exposed_rep2"
+    dataset = datab.get_set("GSM576086")
+    display_graphics(dataset)
+
+    print "Liver_D_Ref_rep2"
+    dataset = datab.get_set("GSM576084")
+    display_graphics(dataset)
+
+    print "Liver_E_Exposed_rep2"
+    dataset = datab.get_set("GSM576081")
+    display_graphics(dataset)
+
+    print "Testis_UF_Control_rep2"
+    dataset = datab.get_set("GSM576102")
+    display_graphics(dataset)
+
+    print "Testis_A_Exposed_rep2"
+    dataset = datab.get_set("GSM576103")
+    display_graphics(dataset)
+
+    print "Testis_B_Exposed_rep2"
+    dataset = datab.get_set("GSM576106")
+    display_graphics(dataset)
+
+    print "Testis_D_Ref_rep3"
+    dataset = datab.get_set("GSM576100")
+    display_graphics(dataset)
+
+    print "Testis_E_Exposed_rep2"
+    dataset = datab.get_set("GSM576105")
+    display_graphics(dataset)
